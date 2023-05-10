8e41bff4f5c31845dce84750f744ca0ea1c27825
Sébastien NEDJAR <sebastien@nedjar.com>
2023-05-10 09:15:00 +0200

    Create basic Express server
    🌐 Basic web server setup
    - Simple Express.js server listening on port 3000
    - Basic route for platform homepage

--- src/index.js
+++ src/index.js
@@ -2,19 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware
-app.use(express.json());
-app.use(express.static("public"));
-
-// Routes
-app.use("/api/sensors", require("./routes/sensors"));
-
-// Health check
-app.get("/api/health", (req, res) => {
-    res.json({ status: "OK", message: "SteamCity IoT Platform is running" });
+app.get("/", (req, res) => {
+    res.send("SteamCity IoT Platform - Coming Soon");
 });
 
 app.listen(PORT, () => {
-    console.log(`SteamCity Sensor Data API running on port ${PORT}`);
-    console.log(`Visit http://localhost:${PORT} for API information`);
+    console.log(`Server running on port ${PORT}`);
 });
